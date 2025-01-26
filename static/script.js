aecd4202b84d90db2cdd529f71600758b1ccb95b
juorme_home <gaminjuniorortiz@gmail.com>
2025-01-26 01:14:01 -0500

    update parrafo presentacion

--- static/script.js
+++ static/script.js
@@ -1,24 +1,3 @@
-// Obtener referencia al elemento de navegación //
-const navLinks = document.querySelectorAll('nav a');
-
-
-// Agregar evento de clic a cada enlace de navegación //
-navLinks.forEach(link => {
-    link.addEventListener('click', scrollToSection);
+document.addEventListener('DOMContentLoaded', () => {
+    // JavaScript for interactivity or small animations
 });
-
-
-// Función para desplazarse suavemente a una sección
-function scrollToSection(event) {
-    event.preventDefault();
-
-    const targetId = event.target.getAttribute('href');
-    const targetSection = document.querySelector(targetId);
-
-    if (targetSection) {
-        window.scrollTo({
-            top: targetSection.offsetTop,
-            behavior: 'smooth'
-        });
-    }
-}
